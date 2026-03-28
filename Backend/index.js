5e8f347877cf544c7e0ffcfe3183533851f69c41
LIKHITHH-HUB <shivaadas15@gmail.com>
2026-03-28 23:13:31 +0530

    fix port for deployement

--- Backend/index.js
+++ Backend/index.js
@@ -146,6 +146,11 @@ app.post('/updatepost',(req,res)=>{
         } 
     )
 });
-app.listen(3000,()=>{
-    console.log('server started on port 3000!')
-})
+// app.listen(3000,()=>{
+//     console.log('server started on port 3000!')
+// })
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () =>{
+    console.log(`server started on port $(PORT)`);
+});
